48399da58ebf4800eb5fbc51711ea3f7bc30a7cc
toto001011 <115989024+toto001011@users.noreply.github.com>
2022-12-13 11:47:36 +0100

    socket con Callable
    Inizio implementazione gestione della ricezione dei socket con Callable

---
+++ 2022-11-25 -- Inizio Sviluppo Gui CLIENT MAIL/src/main/java/com/example/fx/ui/ServerTasks.java
@@ -0,0 +1,27 @@
+package com.example.fx.ui;
+/*
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.Callable;
+import java.net.ServerSocket;
+public class ServerTasks implements Callable<Socket> {
+
+    int port;//porta alla quale si connette il socket
+    public ServerTasks(int port){this.port=port;}
+
+    @Override
+    public Socket call() throws IOException {
+
+
+        ServerSocket s = new ServerSocket(port);
+
+        Socket income =s.accept();
+        System.out.println("INIZIO computazione " );
+
+        System.out.println("FINE computazione " + ": risultato = " );
+
+
+
+        return income;
+    }
+}*/
